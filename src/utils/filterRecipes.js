3da44b7d69db205b04a90c5e9ae9bd7861cdcca4
DevWeb13 <giuliano.loic@sfr.fr>
2022-07-22 09:57:02 +0200

    refactor: (filterRecipes.js): refactor function filterBySearch

--- src/utils/filterRecipes.js
+++ src/utils/filterRecipes.js
@@ -1,9 +1,7 @@
 /**
  * It filters the recipes by the tags
- *
  * @param   {array}  recipes  - The list of recipes
  * @param   {array}  tags     - The list of tags
- *
  * @return  {array}           - The list of recipes that match the tags
  */
 function filterByTags(recipes, tags) {
@@ -39,35 +37,28 @@ function filterByTags(recipes, tags) {
  * that match the search term
  * @param   {array}  recipes  - The list of recipes
  * @param   {string}  search   - The search input value
- *
  * @return  {Array}           - The list of recipes that match the search criteria
  */
 function filterBySearch(recipes, search) {
-  let filteredRecipes = [];
   const searchTerm = search.toLowerCase().trim();
-  if (search.length < 3) {
-    filteredRecipes = recipes;
-  } else {
-    filteredRecipes = recipes.filter((recipe) => {
-      return (
-        recipe.name.toLowerCase().includes(searchTerm) ||
-        recipe.description.toLowerCase().includes(searchTerm) ||
-        recipe.ingredients.some((ingredient) =>
-          ingredient.ingredient.toLowerCase().includes(searchTerm),
-        )
-      );
-    });
-  }
-  return filteredRecipes;
+  if (searchTerm.length < 3) return recipes;
+  return recipes.filter((recipe) => {
+    return (
+      recipe.name.toLowerCase().includes(searchTerm) ||
+      recipe.description.toLowerCase().includes(searchTerm) ||
+      recipe.ingredients.some((ingredient) =>
+        ingredient.ingredient.toLowerCase().includes(searchTerm),
+      )
+    );
+  });
 }
 
 /**
- * [filterRecipes description]
+ * It filters recipes by search and tags
  *
  * @param   {array}  recipes  - The list of recipes
  * @param   {string}  search   - The search input value
  * @param   {array}  tags     - The list of tags
- *
  * @return  {array}             - The list of recipes that match the search criteria and the tags
  */
 export default function filterRecipes(recipes, search, tags) {
