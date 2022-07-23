5a1994c495fb53c912fd6d8a04db114dcd4820be
DevWeb13 <giuliano.loic@sfr.fr>
2022-07-23 12:02:21 +0200

    feat: (filterRecipes.js): function filterBySearch with native loops

--- src/utils/filterRecipes.js
+++ src/utils/filterRecipes.js
@@ -32,25 +32,49 @@ function filterByTags(recipes, tags) {
   return filteredRecipes;
 }
 
-/**
- * If the search term is less than 3 characters, return all recipes, otherwise return only the recipes
- * that match the search term
- * @param   {array}  recipes  - The list of recipes
- * @param   {string}  search   - The search input value
- * @return  {Array}           - The list of recipes that match the search criteria
- */
+/* **************************** Version with array object methods ********************** */
+// /**
+//  * If the search term is less than 3 characters, return all recipes, otherwise return only the recipes
+//  * that match the search term
+//  * @param   {array}  recipes  - The list of recipes
+//  * @param   {string}  search   - The search input value
+//  * @return  {Array}           - The list of recipes that match the search criteria
+//  */
+// function filterBySearch(recipes, search) {
+//   const searchTerm = search.toLowerCase().trim();
+//   if (searchTerm.length < 3) return recipes;
+//   return recipes.filter((recipe) => {
+//     return (
+//       recipe.name.toLowerCase().includes(searchTerm) ||
+//       recipe.description.toLowerCase().includes(searchTerm) ||
+//       recipe.ingredients.some((ingredient) =>
+//         ingredient.ingredient.toLowerCase().includes(searchTerm),
+//       )
+//     );
+//   });
+// }
+
+/* **************************** Version with native loops ********************** */
 function filterBySearch(recipes, search) {
   const searchTerm = search.toLowerCase().trim();
   if (searchTerm.length < 3) return recipes;
-  return recipes.filter((recipe) => {
-    return (
-      recipe.name.toLowerCase().includes(searchTerm) ||
-      recipe.description.toLowerCase().includes(searchTerm) ||
-      recipe.ingredients.some((ingredient) =>
-        ingredient.ingredient.toLowerCase().includes(searchTerm),
-      )
-    );
-  });
+  const filteredRecipes = [];
+  console.log(recipes.length);
+  for (let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
+    for (let j = 0; j < recipe.ingredients.length; j++) {
+      const ingredient = recipe.ingredients[j];
+      if (
+        ingredient.ingredient.toLowerCase().includes(searchTerm) ||
+        recipe.name.toLowerCase().includes(searchTerm) ||
+        recipe.description.toLowerCase().includes(searchTerm)
+      ) {
+        filteredRecipes.push(recipe);
+        break;
+      }
+    }
+  }
+  return filteredRecipes;
 }
 
 /**
